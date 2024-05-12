8a0709fe0ef1ee2298b1034dd675740f8702986a
RENATU BONFIM <renatu_mb@yahoo.com.br>
2024-05-12 16:30:59 -0700

    INITIAL COMMIT

---
+++ project-manager-backend/src/main/java/com/renatobonfim/projectmanagerbackend/ProjectManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.renatobonfim.projectmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagerBackendApplication.class, args);
+    }
+
+}
---
+++ project-manager-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<style scoped>
+</style>
